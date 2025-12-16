a68a672967e613185064cf9f64ed0629ecfaf2ac
James <139070656+JamesFnX-ALttPR@users.noreply.github.com>
2025-12-16 14:39:06 -0800

    inputs to sm

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -1,2 +1,2 @@
 @props(['value' => ''])
-<input {{ $attributes->merge(['class' => 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-md p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) }} value="{{ $value }}" />
+<input {{ $attributes->merge(['class' => 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-sm p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) }} value="{{ $value }}" />
